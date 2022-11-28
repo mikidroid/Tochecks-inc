eb8a63d060b5cde12ae110c2688077993cd0ac60
Michael Uche <michaelphardy@gmail.com>
2022-11-28 16:54:22 +0100

    Updates

---
+++ precache-manifest.9219e6787f68fc27ba79b84a48c62a6b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b10c5f0140619d06cc2c53d4ab1112",
+    "url": "/Tochex-inc/index.html"
+  },
+  {
+    "revision": "58de27274be729bcbce2",
+    "url": "/Tochex-inc/static/css/2.487f1fbf.chunk.css"
+  },
+  {
+    "revision": "b5a694436a8b413099c2",
+    "url": "/Tochex-inc/static/css/main.14e5966e.chunk.css"
+  },
+  {
+    "revision": "58de27274be729bcbce2",
+    "url": "/Tochex-inc/static/js/2.6352b70c.chunk.js"
+  },
+  {
+    "revision": "b33930c2996f8e2446a7",
+    "url": "/Tochex-inc/static/js/3.82ffff18.chunk.js"
+  },
+  {
+    "revision": "b5a694436a8b413099c2",
+    "url": "/Tochex-inc/static/js/main.d0d7ecd2.chunk.js"
+  },
+  {
+    "revision": "8ce8e66c383ba89e2cd6",
+    "url": "/Tochex-inc/static/js/runtime~main.d70b1dba.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Tochex-inc/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Tochex-inc/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Tochex-inc/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Tochex-inc/static/media/slick.f97e3bbf.svg"
+  }
+]);
